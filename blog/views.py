3bc3d760fd96b1b4b727b84797f3332756896487
Jack Tattersall <jacktattersall1@gmail.com>
2017-03-29 18:20:59 +0100

    Added markdown converter to blog body

--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,7 @@ from .models import Post, Comment
 from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
 from taggit.models import Tag
+from django.db.models import Count
 
 #
 # class PostListView(ListView):
@@ -12,6 +13,7 @@ from taggit.models import Tag
 #     context_object_name = 'posts'
 #     paginate_by = 3
 #     template_name = 'blog/post/list.html'
+
 
 def post_list(request, tag_slug=None):
     object_list = Post.published.all()
@@ -59,11 +61,17 @@ def post_detail(request, year, month, day, post):
     else:
         comment_form = CommentForm()
 
+    # list of similar posts
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
+
     return render(request,
                   'blog/post/detail.html',
                   {'post': post,
                    'comments': comments,
-                   'comment_form': comment_form})
+                   'comment_form': comment_form,
+                   'similar_posts': similar_posts})
 
 
 def post_share(request, post_id):
